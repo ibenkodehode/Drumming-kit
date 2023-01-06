4b4b0a706b8eaf41c073eb719fdf36b41e919abd
Iben Kodehode <112859791+ibenkodehode@users.noreply.github.com>
2023-01-06 09:29:41 +0100

    Republishing, first commit 22 nov

---
+++ script.js
@@ -0,0 +1,27 @@
+const drumElement = document.querySelector("#drums");
+const keys = ["a", "s", "d", "f", "g", "h", "j"];
+const sounds = [
+  "clap.wav",
+  "hihat.wav",
+  "kick.wav",
+  "openhat.wav",
+  "ride.wav",
+  "snare.wav",
+  "tink.wav",
+  "tom.wav",
+];
+
+for (let i in sounds) {
+  let btn = document.createElement("button");
+  btn.textContent = "♪";
+  btn.setAttribute("src", `/sounds/${sounds[i]}`);
+  btn.onclick = () => {
+    new Audio(`/sounds/${sounds[i]}`).play();
+  };
+  window.addEventListener("keydown", (event) => {
+    if (event.isComposing || event.key == keys[i]) {
+      new Audio(`/sounds/${sounds[i]}`).play();
+    } else null;
+  });
+  drumElement.append(btn);
+}
